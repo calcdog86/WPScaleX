87cb649f900b9e56288abe3afd307da80eb57480
Sebastian Meisel <calcdog@gmail.com>
2025-02-11 14:56:24 +0100

    changed variables

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "webVPC" {
-    cidr_block = "${vpc_cidr_block}"
+    cidr_block = var.vpc_cidr_block
     enable_dns_support = true #gives you an internal domain name
     enable_dns_hostnames = true #gives you an internal host name
     instance_tenancy = "default"    
@@ -10,9 +10,9 @@ resource "aws_vpc" "webVPC" {
 
 resource "aws_subnet" "webSub1" {
     vpc_id = "${aws_vpc.webVPC.id}"
-    cidr_block = "${subnet_1_cidr}"
+    cidr_block = var.subnet_1_cidr
     map_public_ip_on_launch = true //it makes this a public subnet
-    availability_zone = "${subnet_1_az}"
+    availability_zone = var.subnet_1_az
     tags = {
         Name = "webSub1"
     }
@@ -20,9 +20,9 @@ resource "aws_subnet" "webSub1" {
 
 resource "aws_subnet" "webSub2" {
     vpc_id = "${aws_vpc.webVPC.id}"
-    cidr_block = "${subnet_2_cidr}"
+    cidr_block = var.subnet_2_cidr
     map_public_ip_on_launch = true //it makes this a public subnet
-    availability_zone = "${subnet_2_az}"
+    availability_zone = var.subnet_2_az
     tags = {
         Name = "webSub2"
     }
@@ -30,9 +30,9 @@ resource "aws_subnet" "webSub2" {
 
 resource "aws_subnet" "webSub3" {
     vpc_id = "${aws_vpc.webVPC.id}"
-    cidr_block = "${subnet_1_cidr}"
+    cidr_block = var.subnet_1_cidr
     map_public_ip_on_launch = true //it makes this a public subnet
-    availability_zone = "${subnet_3_az}"
+    availability_zone = var.subnet_3_az
     tags = {
         Name = "webSub3"
     }
